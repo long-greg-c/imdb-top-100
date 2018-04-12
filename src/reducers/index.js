41c234e96ba152a9c0bc476be82354cda703bf0b
Greg Long <greg@jongensvantechniek.nl>
2018-04-11 20:13:50 -0700

    Implement Redux reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
-import score from './score';
-import visibilityFilter from './visibilityFilter';
+import ScoreReducer from './reducer_score';
+import MovieListReducer from './reducer_movie_list';
+import GameStateReducer from './reducer_game_state';
+import RequestedRankReducer from './reducer_requested_rank';
+import LoadingStateReducer from './reducer_loading_state';
 
 export default combineReducers({
-  score,
-  visibilityFilter
+  score: ScoreReducer,
+  movieList: MovieListReducer,
+  gameState: GameStateReducer,
+  requestedRank: RequestedRankReducer,
+  loadingState: LoadingStateReducer
 });
