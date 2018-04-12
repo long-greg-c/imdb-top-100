eb53477c33754d9a9dd4f3b816484c8d2eb4d2d0
Greg Long <greg@jongensvantechniek.nl>
2018-04-11 20:13:02 -0700

    Implement Redux Store provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import ReduxPromise from 'redux-promise';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk, ReduxPromise)(
+  createStore
+);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
